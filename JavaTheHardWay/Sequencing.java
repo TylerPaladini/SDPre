e1b4e6ff2a3ef43be0ad4ef03d103b32b38b8ca5
Tyler Paladini <TylerPaladini@yahoo.com>
2018-07-30 20:56:22 -0600

    Exercise 10

--- JavaTheHardWay/Sequencing.java
+++ JavaTheHardWay/Sequencing.java
@@ -2,23 +2,18 @@ import java.util.Scanner;
 
 public class Sequencing {
   public static void main(String[] args) {
-    // THIS CODE IS BROKEN UNTIL YOU FIX import junit.framework.TestCase;
 
     Scanner keyboard = new Scanner(System.in);
-    double price, salesTax, total;
+    double price = 0, salesTax, total;
 
-    salesTax = price *0.0825;
-    total = price + salesTax;
-
-    System.out.print( "How much is the purchase price? ");
+    System.out.print( "How much is the purchase price?" );
     price = keyboard.nextDouble();
 
-    System.out.println( " Item price:\t" + price );
-    System.out.println( " Sales tax:\t" + salesTax);
-    System.out.println( " Total cost:\t" + total );
+    salesTax = price * 0.0825;
+    total = price + salesTax;
 
-
+    System.out.println( "Item price:\t" + price);
+    System.out.println( "Sales tax:\t" + salesTax );
+    System.out.println( "Total cost:\t" + total);
   }
 }
-
-// Compiles, but does not initialize price because it is not defined. 
